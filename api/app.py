1bc4b2453d1a81e47f29363fb083517ab6a74f27
noor-nimrat <noornimrat00@icloud.com>
2025-07-22 20:27:22 +0530

    fix: module not found error

--- api/app.py
+++ api/app.py
@@ -13,7 +13,29 @@ from io import BytesIO
 import json
 import logging
 import requests
-from errorUtil import get_jsonified_error
+
+ERRORS = {
+    "TOO_MANY_REQUESTS": {
+        "status": 429,
+        "message": "You have reached the maximum number of requests allowed. Please try again later."
+    },
+    "PINTEREST_UNAUTHORIZED": {
+        "status": 401,
+        "message": "You are not authorized to access this Pinterest board. Redirecting..."
+    },
+    "UNKNOWN_ERROR": {
+        "status": 500,
+        "message": "An unknown error occurred. Please try again later."
+    }
+}
+
+def get_jsonified_error(error_name):
+    if error_name in ERRORS:
+        error = ERRORS[error_name]
+    else:
+        error = ERRORS["UNKNOWN_ERROR"]
+    return jsonify({ "error": error["message"] }), error["status"]
+
 
 # from dotenv import load_dotenv
 load_dotenv() # Load environment variables from .env file
