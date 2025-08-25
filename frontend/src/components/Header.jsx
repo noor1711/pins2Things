5758f508486073868422a2def18e0319bf782647
noor-nimrat <noornimrat00@icloud.com>
2025-08-25 22:38:20 +0530

    fix: added responsiveness to website

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="flex font-bold drop-shadow-5xl text-8xl sm:text-6xl md:text-7xl flex-row items-center justify-center text-[#f03a91e8] hover:text-[#fa0279] text-shadow-lg/50 text-shadow-neutral-500 transition-transform duration-200 hover:-translate-y-0.5 font-sourgummy">
+    <div className="flex font-bold drop-shadow-5xl text-5xl lg:text-8xl md:text-6xl sm:text-5xl flex-row items-center justify-center text-[#f03a91e8] hover:text-[#fa0279] text-shadow-lg/50 text-shadow-neutral-500 transition-transform duration-200 hover:-translate-y-0.5 font-sourgummy">
       <h1>Pins2Things</h1>
     </div>
   );
