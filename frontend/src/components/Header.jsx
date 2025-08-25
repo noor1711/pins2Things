da64fe1601524fbc6a6b57e2355deb43e0d774e2
noor-nimrat <noornimrat00@icloud.com>
2025-08-25 22:34:13 +0530

    feat: added responsiveness to website

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,10 +3,8 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="flex flex-row items-center justify-center text-[#f03a91e8] hover:text-[#fa0279] text-shadow-lg/50 text-shadow-neutral-500 transition-transform duration-200 hover:-translate-y-0.5 font-sourgummy">
-      <h1 className="text-8xl font-bold drop-shadow-5xl">Pins</h1>
-      <h1 className="text-9xl font-bold drop-shadow-5xl">2</h1>
-      <h1 className="text-8xl font-bold drop-shadow-5xl">Things</h1>
+    <div className="flex font-bold drop-shadow-5xl text-8xl sm:text-6xl md:text-7xl flex-row items-center justify-center text-[#f03a91e8] hover:text-[#fa0279] text-shadow-lg/50 text-shadow-neutral-500 transition-transform duration-200 hover:-translate-y-0.5 font-sourgummy">
+      <h1>Pins2Things</h1>
     </div>
   );
 };
